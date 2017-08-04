13f57640bcbfaeefdbb23d4038db15d0e86fe49e
Martin Chan <martin.chan@red-gate.com>
2017-08-04 17:00:40 +0100

    code kata refactoring thing

--- tests/Tdd.Exercise7.Tests/GameShould.cs
+++ tests/Tdd.Exercise7.Tests/GameShould.cs
@@ -25,8 +25,8 @@ namespace Tdd.Exercise7.Tests
         [Test]
         public void Decide_the_winner_based_on_round_results()
         {
-            _player1.RevealHand().Returns(Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Scissors);
+            _player1.RevealHand().Returns(new Paper());
+            _player2.RevealHand().Returns(new Scissors());
 
             GameResult result = _game.Play(_player1, _player2);
             result.WinningPlayer.ShouldBe(_player2);
@@ -35,8 +35,9 @@ namespace Tdd.Exercise7.Tests
         [Test]
         public void Decide_the_winner_after_at_least_three_rounds()
         {
-            _player1.RevealHand().Returns(Hand.Paper, Hand.Paper, Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Scissors, Hand.Rock, Hand.Rock);
+            _player1.RevealHand().Returns(new Paper(), new Paper(), new Paper());
+            _player2.RevealHand().Returns(new Scissors(), new Rock(), new Rock());
+
 
             GameResult result = _game.Play(_player1, _player2);
             result.RoundCount.ShouldBe(3);
@@ -46,8 +47,8 @@ namespace Tdd.Exercise7.Tests
         [Test]
         public void Continue_until_there_is_a_clear_winner()
         {
-            _player1.RevealHand().Returns(Hand.Paper, Hand.Paper, Hand.Paper, Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Rock, Hand.Scissors, Hand.Paper, Hand.Rock);
+            _player1.RevealHand().Returns(new Paper(), new Paper(), new Paper(), new Paper());
+            _player2.RevealHand().Returns(new Rock(), new Scissors(), new Paper(), new Rock());
 
             GameResult result = _game.Play(_player1, _player2);
             result.RoundCount.ShouldBe(4);
@@ -57,8 +58,8 @@ namespace Tdd.Exercise7.Tests
         [Test]
         public void Continue_until_the_first_winner_after_three_draws()
         {
-            _player1.RevealHand().Returns(Hand.Paper, Hand.Paper, Hand.Paper, Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Paper, Hand.Paper, Hand.Paper, Hand.Rock);
+            _player1.RevealHand().Returns(new Paper(), new Paper(), new Paper(), new Paper());
+            _player2.RevealHand().Returns(new Paper(), new Paper(), new Paper(), new Rock());
 
             GameResult result = _game.Play(_player1, _player2);
             result.RoundCount.ShouldBe(4);
